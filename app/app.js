75bb7139522cae8c89d6ce4ae7e8b8a5328ad0cf
jackcobb <jack3@vt.edu>
2016-10-01 10:14:49 -0400

    Updating web pack to create source map and display home state

--- app/app.js
+++ app/app.js
@@ -1,1 +1,15 @@
-var app = angular.module("website", ["ui.router"]); 
+var app = angular.module('website', ['ui.router']); 
+
+app.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider.state('home', {
+    	views : {
+    		'mainView' : {
+    			template: '<h1>hello</h1>'
+    		}
+    	},
+        url: '/home'       
+    });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   entry: ["./app/entry.js", "./app/app.js"],
+  devtool: 'source-map',
   output: {
     filename: "dist/bundle.js"
   }
